b00d8df80fc357ed06e84c9415729a0f87d49f04
Imamuzzaki <imamuzzaki@gmail.com>
2021-08-07 14:50:48 +0700

    Update blade files to display error messages

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -12,8 +12,7 @@
                 <form method="POST" action="/task/{{ $task->id }}">
 
                     <div class="form-group">
-                        <textarea name="description"
-                            class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white">{{ $task->description }}</textarea>
+                        <textarea name="description" class="form-control">{{ $task->description }}</textarea>
                         @if ($errors->has('description'))
                             <span class="text-danger">{{ $errors->first('description') }}</span>
                         @endif
